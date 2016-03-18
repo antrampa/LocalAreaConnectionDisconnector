7e180ab16eb9a758e0de8d30cb0608e22f643226
a.zalonis <a.zalonis@sigmasolutions.gr>
2016-03-18 11:46:14 +0200

    Initial commit

---
+++ LocalAreaConnectionDisconnector/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace LocalAreaConnectionDisconnector
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loopCheckBox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusLable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bttnStart
+            // 
+            this.bttnStart.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.bttnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnStart.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.bttnStart.Location = new System.Drawing.Point(39, 166);
+            this.bttnStart.Name = "bttnStart";
+            this.bttnStart.Size = new System.Drawing.Size(381, 55);
+            this.bttnStart.TabIndex = 0;
+            this.bttnStart.Text = "Start Playing";
+            this.bttnStart.UseVisualStyleBackColor = false;
+            this.bttnStart.Click += new System.EventHandler(this.bttnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(40, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Switching per seconds:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // loopCheckBox
+            // 
+            this.loopCheckBox.AutoSize = true;
+            this.loopCheckBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.loopCheckBox.Location = new System.Drawing.Point(341, 98);
+            this.loopCheckBox.Name = "loopCheckBox";
+            this.loopCheckBox.Size = new System.Drawing.Size(50, 17);
+            this.loopCheckBox.TabIndex = 3;
+            this.loopCheckBox.Text = "Loop";
+            this.loopCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(42, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Local Area Status: ";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(39, 42);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(378, 26);
+            this.nameTextBox.TabIndex = 6;
+            this.nameTextBox.Text = "Local Network Connection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Local Area Name";
+            // 
+            // statusLable
+            // 
+            this.statusLable.AutoSize = true;
+            this.statusLable.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.statusLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLable.ForeColor = System.Drawing.Color.Red;
+            this.statusLable.Location = new System.Drawing.Point(198, 131);
+            this.statusLable.Name = "statusLable";
+            this.statusLable.Size = new System.Drawing.Size(66, 22);
+            this.statusLable.TabIndex = 7;
+            this.statusLable.Text = "Status ";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = global::LocalAreaConnectionDisconnector.Properties.Resources.ambitos;
+            this.ClientSize = new System.Drawing.Size(483, 271);
+            this.Controls.Add(this.statusLable);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.loopCheckBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bttnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(483, 271);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(483, 271);
+            this.Name = "mainForm";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Local Area Disconnector";
+            this.TransparencyKey = System.Drawing.SystemColors.Info;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox loopCheckBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statusLable;
+    }
+}
+
---
+++ LocalAreaConnectionDisconnector/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+
+namespace LocalAreaConnectionDisconnector
+{
+    public partial class mainForm : Form
+    {
+        #region Fields 
+        private string _name;
+        private string _status;
+        #endregion
+
+        #region Constructors 
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods 
+        private void EnableDisableInNewThread()
+        {
+            Thread newThread = new Thread(EnableDisable);
+            newThread.Start();
+
+        }
+        private void EnableDisable()
+        {
+            try
+            {
+                _name = nameTextBox.Text;
+                SelectQuery wmiQuery = new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionId != NULL");
+                ManagementObjectSearcher searchProcedure = new ManagementObjectSearcher(wmiQuery);
+                foreach (ManagementObject item in searchProcedure.Get())
+                {
+                    if (((string)item["NetConnectionId"]) == _name) //"Local Network Connection"
+                    {
+                        if(_status == "Disable")
+                        {
+                            item.InvokeMethod("Enable", null);
+                            _status = "Enable";
+                        }
+                        else
+                        {
+                            item.InvokeMethod("Disable", null);
+                            _status = "Disable";
+                        }
+
+                        if(statusLable.InvokeRequired)
+                        {
+                            //label5.Invoke((MethodInvoker)(() => label5.Text = "Requested" + repeats + "Times"));
+                            statusLable.Invoke((MethodInvoker)(() => statusLable.Text = _status));
+                        }
+                        else
+                        {
+                            statusLable.Text = _status;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+        }
+        #endregion
+
+        #region Events 
+        private void bttnStart_Click(object sender, EventArgs e)
+        {
+            EnableDisableInNewThread();
+        }
+        #endregion
+    }
+}
